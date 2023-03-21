001d1725d7315bf6d308d51a913dd31c4d7b35e2
r0r-5chach <joshualukekillen@gmail.com>
2023-03-21 00:05:46 +0000

    converted generated_ui to templates

---
+++ templates/patientsprofile.html.php
@@ -0,0 +1,56 @@
+    <section class="u-align-center u-clearfix u-white u-section-1" id="sec-6ca6">
+      <div class="u-clearfix u-sheet u-valign-middle-lg u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-sheet-1">
+        <h1 class="u-custom-font u-font-raleway u-text u-text-default u-text-palette-1-base u-text-1">Hello!</h1>
+        <p class="u-large-text u-text u-text-variant u-text-2">welcome to the profile page</p>
+        <div class="u-align-left u-container-style u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-group u-palette-1-base u-radius-20 u-shape-round u-group-1">
+          <div class="u-container-layout u-valign-middle-md u-valign-middle-sm u-valign-middle-xs u-container-layout-1">
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-default u-text-3">name</h6>
+            <p class="u-custom-font u-text u-text-4">Fred Morrison</p>
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-default u-text-5">Birthday</h6>
+            <p class="u-custom-font u-text u-text-6">25. 05. 1989</p>
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-default u-text-7">Diagnosis</h6>
+            <p class="u-custom-font u-text u-text-8">Lead Designer</p>
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-default u-text-9">Email</h6>
+            <a href="https://nicepage.one" class="u-border-1 u-border-active-palette-1-light-2 u-border-hover-palette-1-light-2 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-none u-text-active-palette-1-light-2 u-text-body-alt-color u-text-hover-palette-1-light-2 u-btn-1">designer@example.com</a>
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-10">Phone</h6>
+            <a href="tel:+987987654321" class="u-border-1 u-border-active-palette-2-base u-border-hover-palette-2-base u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-none u-text-active-palette-2-base u-text-body-alt-color u-text-hover-palette-2-base u-btn-2">(+987) 987 654 321</a>
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-default u-text-11">NHS ID</h6>
+            <a href="https://nicepage.app" class="u-border-1 u-border-active-palette-1-light-2 u-border-hover-palette-1-light-2 u-border-no-left u-border-no-right u-border-no-top u-btn u-button-style u-none u-text-active-palette-1-light-2 u-text-body-alt-color u-text-hover-palette-1-light-2 u-btn-3" data-animation-name="" data-animation-duration="0" data-animation-delay="0" data-animation-direction="">28288283838</a>
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-12">location</h6>
+            <p class="u-custom-font u-text u-text-13">New York, USA</p>
+            <h6 class="u-custom-font u-font-open-sans u-text u-text-default u-text-14">Interests</h6>
+            <p class="u-custom-font u-text u-text-15">Games, Books, Movies</p>
+          </div>
+        </div>
+        <img class="u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-round u-radius-20 u-image-1" alt="" data-image-width="997" data-image-height="700" src="images/ghghhgh.jpg">
+        <div class="u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-list u-list-1">
+          <div class="u-repeater u-repeater-1">
+            <div class="u-align-left u-container-style u-list-item u-repeater-item u-shape-rectangle">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-2">
+                <p class="u-text u-text-default u-text-palette-1-dark-2 u-text-16">Referral 1</p>
+                <h3 class="u-custom-font u-font-montserrat u-text u-text-default u-text-palette-1-base u-text-17" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">17</h3>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item u-shape-rectangle">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-3">
+                <p class="u-text u-text-default u-text-palette-1-dark-2 u-text-18"> Referral 2</p>
+                <h3 class="u-custom-font u-font-montserrat u-text u-text-default u-text-palette-1-base u-text-19" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">12+</h3>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item u-shape-rectangle">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-4">
+                <p class="u-text u-text-default u-text-palette-1-dark-2 u-text-20"> Referral 4</p>
+                <h3 class="u-custom-font u-font-montserrat u-text u-text-default u-text-palette-1-base u-text-21" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">108</h3>
+              </div>
+            </div>
+            <div class="u-align-left u-container-style u-list-item u-repeater-item u-shape-rectangle">
+              <div class="u-container-layout u-similar-container u-valign-middle u-container-layout-5">
+                <p class="u-text u-text-default u-text-palette-1-dark-2 u-text-22"> Referral 3</p>
+                <h3 class="u-custom-font u-font-montserrat u-text u-text-default u-text-palette-1-base u-text-23" data-animation-name="counter" data-animation-event="scroll" data-animation-duration="3000">150</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a href="mailto:hi@freedesigner.com" class="u-active-palette-1-light-2 u-border-none u-btn u-btn-round u-button-style u-hover-palette-1-light-2 u-palette-1-base u-radius-8 u-text-active-white u-text-body-alt-color u-text-hover-white u-btn-4">EDIT PROFILE </a>
+      </div>
+    </section>
