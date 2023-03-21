fcb2a302bbf8c466893b4f8d1f49a15ce1029d31
kolade082 <101345199+kolade082@users.noreply.github.com>
2023-03-21 00:49:00 +0000

    Add files via upload

---
+++ Controller/User.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Controller;
+
+use site\DatabaseTable;
+
+class User
+{
+    private $userTable;
+
+    private $validator;
+
+    public function __construct(DatabaseTable $userTable) {
+        $this->userTable = $userTable;
+        $validator = new Validations();
+        $this->validator = $validator;
+    }
+
+    public function login(){
+            return ['template' => 'login.html.php',
+                'title' => 'Login',
+                'vars' => []];
+    }
+    public function loginSubmit()
+    {
+        $template = 'login.html.php';
+        $errors = [];
+        $message = '';
+
+        if ($_POST) {
+            if (isset($_POST['submit'])) {
+                $email = $_POST['email'] ?? '';
+                $password = $_POST['password'] ?? '';
+
+                $errors = $this->validator->validateLoginForm($email, $password);
+                if (empty($errors)){
+                    $user = $this->userTable->find("email",
+                        $_POST['email']);
+                    if ($user) {
+                        $chkPassword = password_verify($_POST['password'],
+                            $user[0]["password"]);
+                        if ($chkPassword) {
+                            //  valid
+                            $this->session();
+                            $_SESSION['loggedin'] = $user[0]['id'];
+                            $_SESSION['userDetails'] = $user[0];
+
+                            header('Location: home');
+                        } else {
+                            $message = "Invalid Cred"; // password
+                        }
+                    } else {
+                        $message = "Invalid Cred"; // username
+                    }
+                }
+            }
+        }
+        return ['template' => $template, 'title' => 'login',
+            'vars' => ['errors' => $errors,
+                'message' => $message]];
+    }
+
+    public function logout()
+    {
+        session_start();
+        session_unset();
+        session_destroy();
+
+        header('Location: index');
+    }
+
+
+
+
+    /**
+     * @return void
+     */
+    public function session()
+    {
+        if (!isset($_SESSION)) {
+            session_start();
+        }
+    }
+}
--- site/Routes.php
+++ site/Routes.php
@@ -2,10 +2,14 @@
 namespace site;
 class Routes extends \framework\Routes {
     public function __construct() {
-        $this->databaseTables = [];
+        $this->databaseTable = [];
         $this->controllers = [];
+        $this->initTables();
         //TODO: Add database tables to array
         //TODO: Add controllers to array
         //TODO: add login controllers to array
+    }
+    public function initTables(){
+        $this->databaseTable['users'] = new \site\DatabaseTable();
     }
 }
